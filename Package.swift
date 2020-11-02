04036aa08a314b7c34e6883375b69ded3516d87e
Praveen <praveen.velanati@sparity.com>
2020-11-02 00:58:54 -0500

    Removed other targets

--- Package.swift
+++ Package.swift
@@ -7,31 +7,14 @@ let package = Package(
     name: "Fuji",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "SportsThemeFoundation", targets: ["SportsThemeFoundation"]),
-        .library(name: "SportsUI", targets: ["SportsTheme"]),
-        .library(name: "ArtsThemeFoundation", targets: ["ArtsThemeFoundation"]),
-        .library(name: "ArtsUI", targets: ["ArtsTheme"])
+        .library(name: "Fuji", type: .dynamic, targets: ["Fuji"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "FujiCoreVisual", url: "https://github.com/praveenvelanati/FujiCoreVisual.git", .branch("main"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "FujiCoreVisual"),
         .target(name: "Fuji",
-            dependencies: ["FujiCoreVisual"]),
-        .target(name: "SportsThemeFoundation",
-                dependencies: ["FujiCoreVisual"]),
-        .target(name: "SportsTheme",
-                dependencies: ["Fuji", "FujiCoreVisual", "SportsThemeFoundation"]),
-        .target(name: "ArtsThemeFoundation",
-                dependencies: ["Fuji", "FujiCoreVisual"]),
-        .target(name: "ArtsTheme",
-                dependencies: ["Fuji", "FujiCoreVisual", "ArtsThemeFoundation"]),
-        .testTarget(
-            name: "FujiTests",
-            dependencies: ["Fuji"]),
+                dependencies: [.product(name: "FujiCoreVisual", package: "FujiCoreVisual")])
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import FujiTests
-
-var tests = [XCTestCaseEntry]()
-tests += FujiTests.allTests()
-XCTMain(tests)
